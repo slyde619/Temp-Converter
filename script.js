25910026142368848ea50c59f50ed526b666cbcf
slyde619 <slyde619@gmail.com>
2020-07-31 20:24:25 +0000

    feature: added clear button

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// Declare UI var
+// Declare UI variable
 const form = document.querySelector('#converter')
-const celciusValue = document.querySelector('#celciusValue')
+const celsiusValue = document.querySelector('#celsiusValue')
 const showFarenheit = document.getElementById('showFarenheit')
 const result = document.querySelector('#results')
+const clearBtn = document.querySelector('.clearBtn')
+let inputCelsius;
+
 // Listen for events
 form.addEventListener('submit', calculateResult)
 // calculate result
 function calculateResult(e){
   // Get celcius value
-  const inputCelcius = celciusValue.value
-  const calculate = (inputCelcius * 9 / 5) + 32
+  inputCelsius = celsiusValue.value
+  const calculate = (inputCelsius * 9 / 5) + 32
   result.style.display = 'block'
   showFarenheit.value = calculate
-  if(inputCelcius === ''){
+  if(inputCelsius === ''){
     showError()
   }
   e.preventDefault()
@@ -20,7 +23,7 @@ function calculateResult(e){
 
 function showError(){
   result.style.display = 'none'
-  // Get showError & card
+  // Get showMsg & card
   const showMsg = document.querySelector('.showMsg')
   const card = document.querySelector('.card')
   // Create an Errordiv
@@ -28,12 +31,18 @@ function showError(){
   // Add a classname
   errorDiv.className = 'alert alert-warning'
   // Add a text and append to the Div element
-  errorDiv.appendChild(document.createTextNode('Please check your inputs!'))
+  errorDiv.appendChild(document.createTextNode('Please check your inputs!!'))
   // Insert error before Header
   showMsg.insertBefore(errorDiv, card)
   // set Timing
-  window.setTimeout(clearError, 1500)
+  window.setTimeout(clearError, 2000)
 }
 function clearError(){
   document.querySelector('.alert').remove()
+}
+// ClearBtn Feature
+clearBtn.addEventListener('click', clearInputs)
+function clearInputs(){
+  celsiusValue.value = ''
+  result.style.display = 'none'
 }
